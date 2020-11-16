b4f8bc7c66d96abb44856c93b56f2dd081f7dbf1
Olimene Oladipupo <dipotope@yahoo.com>
2020-11-16 19:58:52 +0100

    Update locals.tf

--- Terraform/tf-scaffolding-ldz-/locals.tf
+++ Terraform/tf-scaffolding-ldz-/locals.tf
@@ -1,11 +1,9 @@
 locals {
 
   prefix   = "halo-mvct"
- # rg_name  = "demo-rg" #An existing Resource Group for the Application Gateway 
   sku_name = "Standard_Small"
   sku_tier = "Standard"
   capacity = 2
- # subnet_id = "/subscriptions/bb9687aa-6b97-47f1-bd9a-7f43b3b0f42d/resourceGroups/demo-rg/providers/Microsoft.Network/virtualNetworks/vnetrg/subnets/appgwsubnet" #Fill Here a dedicated subnet if for the Application Gateway
 
   appname = "halo-mvct-site"
   backend_address_pool = {
