cde806fe70a32a0cc9a3bd0d9bf0edf0adf627e5
Oladipupo Olimene <dipotope@yahoo.com>
2020-11-19 15:24:38 +0100

    removed insights

--- Terraform/tf-scaffolding-ldz-/az_monitor.tf
+++ Terraform/tf-scaffolding-ldz-/az_monitor.tf
@@ -7,16 +7,3 @@ resource "azurerm_log_analytics_workspace" "oms" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
-
-resource "azurerm_log_analytics_solution" "solution" {
-    solution_name         = "${local.prefix}-container-solution-insights"
-    location              = azurerm_resource_group.resource_group.location
-    resource_group_name   = azurerm_resource_group.resource_group.name
-    workspace_resource_id = azurerm_log_analytics_workspace.oms.id
-    workspace_name        = azurerm_log_analytics_workspace.oms.name
-
-    plan {
-        publisher = "Microsoft"
-        product   = "OMSGallery/ContainerInsights"
-    }
-}
